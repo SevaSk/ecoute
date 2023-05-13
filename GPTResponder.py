02d1f12c763ace448e5a46331fb118b7ca9c0c47
SevaSk <sevaskgit@gmail.com>
2023-05-13 16:04:35 -0400

    GTP response error check

--- GPTResponder.py
+++ GPTResponder.py
@@ -6,11 +6,15 @@ import time
 openai.api_key = OPENAI_API_KEY
 
 def generate_response_from_transcript(transcript):
-    response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-0301",
-            messages=[{"role": "system", "content": create_prompt(transcript)}],
-            temperature = 0.0
-    )
+    try:
+        response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo-0301",
+                messages=[{"role": "system", "content": create_prompt(transcript)}],
+                temperature = 0.0
+        )
+    except openai.OpenAIError:
+        print("An error occurred while calling OpenAI ChatCompletion.")
+        return ''
     full_response = response.choices[0].message.content
     try:
         return full_response.split('[')[1].split(']')[0]
